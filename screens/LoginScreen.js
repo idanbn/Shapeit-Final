192dcf790529f15d501ab2bbb51cfc8ecf0c781c
idan <58389697+idanbn@users.noreply.github.com>
2021-12-09 01:02:20 +0200

    SignOut Function
    navigate to login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,69 +1,83 @@
-import React, {useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet,TextInput, Text, View, Touchable, TouchableOpacity } from 'react-native'
-import { auth} from '../firebase'
-
-
+import { useNavigation } from '@react-navigation/core'
+import React, { useEffect, useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { auth } from '../firebase'
 
 const LoginScreen = () => {
-    
-    
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const handleSignUp = () => {
-        auth
-          .createUserWithEmailAndPassword(email, password)
-          .then(userCredentials => {
-            const user = userCredentials.user;
-            console.log(user.email);
-          })
-          .catch(error => alert(error.message))
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.replace("Home")
       }
-  
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior="padding"
-            >
-             <View style={styles.inputContainer}>
-                 <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    style={styles.input}
-            
-             />
-             <TextInput
-                    placeholder="Password"
-                    value={password }
-                    onChangeText={text => setPassword(text)}
-                    style={styles.input}
-                    secureTextEntry
-            
-             />
-          </View>
+    })
 
+    return unsubscribe
+  }, [])
 
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                onPress={() => { }}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={handleSignUp}
-                style={[styles.button,styles.buttonOutline]}
-            >
-                <Text style={styles.buttonOutlineText}>Register</Text>
-            </TouchableOpacity>
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Registered with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
 
-          </View>
-        </KeyboardAvoidingView>
-    
-        
-    )
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Logged in with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
+
 export default LoginScreen
 
 const styles = StyleSheet.create({
