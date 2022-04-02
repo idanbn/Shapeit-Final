a10f59e14882d4dfd47ed098e28c3452ee2f7f57
faruch ismailov <58651410+farucis@users.noreply.github.com>
2022-04-02 22:05:30 +0300

    add spoonacular api
    -connected api by key
    -add functions to get meals by category
    -add function to get meal informations by ID
    -add function to get meal Nutrition by ID
    -add function to get meal Ingredients by ID
    -UPDATE  in localDB all category names and imaegs

---
+++ API/FoodAPI.js
@@ -0,0 +1,88 @@
+
+//////////////////////////////////// Spoonacular API functions ////////////////////////////////////
+import { config } from "./configuration";
+
+
+const apiURL = 'https://api.spoonacular.com/recipes/'
+
+//--- functions ---//
+
+const MealsbyCategory = async (category = 'pasta') => {
+    const CategoryURL = 'complexSearch?number=8&query='
+
+    const dataURL = apiURL + CategoryURL + category + '&' + config;
+
+    try {
+
+        const response = await fetch(dataURL);
+        const respoJsondata = await response.json();
+
+        return respoJsondata.results;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+}
+
+
+const MealInformation = async (id) => {
+    const InformationURL = '/information?includeNutrition=true'
+    const dataURL = apiURL + id + InformationURL + '&' + config;
+
+    try {
+
+        const response = await fetch(dataURL);
+        const respoJsondata = await response.json();
+
+        console.log(respoJsondata)
+
+        return respoJsondata;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+}
+
+const NutritionByID = async (id) => {
+    const NutritionURL = '/nutritionWidget.json'
+    const dataURL = apiURL + id + NutritionURL + '?' + config;
+
+    try {
+
+        const response = await fetch(dataURL);
+        const respoJsondata = await response.json();
+
+
+        return respoJsondata;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+}
+
+const IngredientsByID = async (id) => {
+    const NutritionURL = '/ingredientWidget.json'
+    const dataURL = apiURL + id + NutritionURL + '?' + config;
+
+    try {
+
+        const response = await fetch(dataURL);
+        const respoJsondata = await response.json();
+
+        return respoJsondata.ingredients;
+
+    } catch (error) {
+        console.error(error);
+
+    }
+
+}
+
+//--- export functions ---//
+export { MealsbyCategory, NutritionByID, IngredientsByID, MealInformation };
--- Localdbs.js
+++ Localdbs.js
@@ -32,6 +32,23 @@ export const Userdb = [
 ];
 
 
+export const LocalCategorysdb = [
+    { name: 'Noodles', image: { uri: 'https://cheapandcheerfulcooking.com/wp-content/uploads/2021/01/chinese-fried-noodles-basic-recipe-1.jpg' } },
+    { name: 'Rice', image: { uri: 'https://www.openaccessgovernment.org/wp-content/uploads/2021/03/dreamstime_xxl_77942944-scaled.jpg' } },
+    { name: 'Chicken', image: { uri: 'https://assets.bonappetit.com/photos/5f809c81ba63e7584fca0576/5:4/w_1405,h_1124,c_limit/Double-Garlic-Roast-Chicken-With-Onion-Gravy.jpg' } },
+    { name: 'Salads', image: { uri: 'https://static.onecms.io/wp-content/uploads/sites/44/2021/02/04/watercress-salad-honey-Balsamic-tofu-2000.jpg' } },
+    { name: 'Pasta', image: { uri: 'https://images.immediate.co.uk/production/volatile/sites/30/2021/04/Pasta-alla-vodka-f1d2e1c.jpg' } },
+    { name: 'Sausages', image: { uri: 'https://www.keziefoods.co.uk/wp-content/uploads/2021/01/Ostrich-Sausages-270g-6-in-a-pack.jpg' } },
+    { name: 'Soups', image: { uri: 'https://www.eatwell101.com/wp-content/uploads/2020/02/chicken-soup-recipe-3.jpg' } },
+    { name: 'Burgers', image: { uri: 'https://insanelygoodrecipes.com/wp-content/uploads/2020/02/Burger-and-Fries.jpg' } },
+    { name: 'Beef', image: { uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnp4vjQp7QCQe09XkOXlO6mMI4gbJuGwyexQ&usqp=CAU' } },
+    { name: 'Vegetarian', image: { uri: 'https://www.beefmagazine.com/sites/beefmagazine.com/files/rocky-mountain-grilled-t-b%20copy_7.png' } },
+    { name: 'Sides', image: { uri: 'https://www.yoranchsteakhouse.com/wp-content/uploads/2018/03/steakhouse-sides-900x600.jpg' } },
+    { name: 'Breakfast', image: { uri: 'https://simply-delicious-food.com/wp-content/uploads/2018/10/breakfast-board.jpg' } },
+    { name: 'Dinner', image: { uri: 'https://cdn77-s3.lazycatkitchen.com/wp-content/uploads/2020/09/basil-tofu-steak-dinner-close-up-1024x1536.jpg' } },
+    { name: 'Lunch', image: { uri: 'https://img.taste.com.au/pfrPC_fT/taste/2020/01/feb20_green-goodness-chicken-sandwich-taste-157311-1.jpg' } },
+    { name: 'Pizza', image: { uri: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F06%2F15%2Fmozzarella-pizza-margherita-FT-RECIPE0621.jpg' } },
+];
 
 export const LocalCategorydb = [
 
@@ -236,14 +253,14 @@ export const LocalCategorydb = [
     {
         c_id: 3,
         text: 'pasta',
-        imageUrl: images.hawaiian_pizza,
+        imageUrl: { uri: 'https://spoonacular.com/recipeImages/654928-312x231.jpg' },
         categoryMeals: [
             {
                 m_id: 1,
-                m_name: 'Spicy Ramen with Mixing Sea Food',
+                m_name: 'Pasta With Tuna',
                 calorie: 223,
                 cookingTime: '20',
-                imageUrl: images.sarawak_laksa,
+                imageUrl: { uri: 'https://spoonacular.com/recipeImages/654959-312x231.jpg' },
                 NutritionalValues: [
                     {
                         protein: 22,
@@ -263,10 +280,10 @@ export const LocalCategorydb = [
             },
             {
                 m_id: 2,
-                m_name: 'Hamburger with friesd',
+                m_name: 'Pasta Margherita',
                 calorie: 760,
                 cookingTime: '25',
-                imageUrl: images.burger_restaurant_1,
+                imageUrl: { uri: 'https://spoonacular.com/recipeImages/511728-312x231.jpg' },
                 NutritionalValues: [
                     {
                         protein: 22,
@@ -286,10 +303,10 @@ export const LocalCategorydb = [
             },
             {
                 m_id: 3,
-                m_name: 'Chicken breast with rice',
+                m_name: 'Pasta and Seafood',
                 calorie: 190,
                 cookingTime: '35',
-                imageUrl: images.ice_kacang,
+                imageUrl: { uri: 'https://spoonacular.com/recipeImages/654812-312x231.jpg' },
                 NutritionalValues: [
                     {
                         protein: 22,
@@ -309,10 +326,10 @@ export const LocalCategorydb = [
             },
             {
                 m_id: 4,
-                m_name: 'Ceaser salad',
+                m_name: 'Pasta On The Border',
                 calorie: 100,
                 cookingTime: '7',
-                imageUrl: images.nasi_lemak,
+                imageUrl: { uri: 'https://spoonacular.com/recipeImages/654857-312x231.jpg' },
                 NutritionalValues: [
                     {
                         protein: 22,
